ce207b97c787949ec1199d1c7c85e283867cd1a5
Akshit Tyagi <akty.tyagi7@gmail.com>
2018-09-12 01:03:49 -0400

    Framework for MDP

--- DecisionProcess.py
+++ DecisionProcess.py
@@ -1,7 +1,7 @@
 import os
 from enum import Enum
 
-from util import ActionSpace
+from util import ActionSpace, GetStateNumber
 
 class MDP():
     
@@ -12,5 +12,24 @@ class MDP():
         self.prob_fail = prob_fail
         self.board = board
 
-    def run(self):
+    def getInitialState(self):
+        return (0, 0)
+    
+    def isTerminalState(self, state):
+        return GetStateNumber(state[0], state[1]) == 23
+
+    def getActionFromPolicy(self, state):
         pass
+
+    def TransitionFunction(self, state, action):
+        pass
+
+    def RewardFunction(self, s_t, a_t, s_t_1):
+        pass 
+
+    def runEpisode(self):
+        state = self.getInitialState()
+        while(not self.isTerminalState(state)):
+            a_t = self.getActionFromPolicy(state)
+            s_t_1 = self.TransitionFunction(state, a_t)
+            r_t = self.RewardFunction(s_t, a_t, s_t_1)
