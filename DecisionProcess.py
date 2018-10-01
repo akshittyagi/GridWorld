b7a78d897ffb01f308fae26feb2f51c2be840e2c
Akshit Tyagi <akty.tyagi7@gmail.com>
2018-10-01 00:17:55 -0400

    Added debug prints

--- DecisionProcess.py
+++ DecisionProcess.py
@@ -13,7 +13,7 @@ from Board import Board
 
 class MDP():
     
-    def __init__(self, board, prob_succ, prob_left_veer, prob_right_veer, prob_fail, gamma):
+    def __init__(self, board, prob_succ, prob_left_veer, prob_right_veer, prob_fail, gamma, debug=False):
         self.prob_succ = prob_succ
         self.prob_left_veer = prob_left_veer
         self.prob_right_veer = prob_right_veer
@@ -22,6 +22,7 @@ class MDP():
         self.dimensions = board.dimensions
         self.actionSpace = {1:"up", 2:"right", 3:"down", 4:"left", 5:"stay"}
         self.gamma = gamma
+        self.debug = debug
 
     def getInitialState(self):
         return (0, 0)
@@ -69,9 +70,11 @@ class MDP():
         return effect, proba
 
     def TransitionFunction(self, state, action):
-        # print "Coming with ACTION: ", self.actionSpace[action], " and at STATE: ", state[0], state[1], " STATE NUMBER ", GetStateNumber(state[0], state[1], self.dimensions)
+        if self.debug:
+            print "Coming with ACTION: ", self.actionSpace[action], " and at STATE: ", state[0], state[1], " STATE NUMBER ", GetStateNumber(state[0], state[1], self.dimensions)
         effect, proba = self.rollTheDice()
-        # print "Effect and Probability val: ", effect, proba
+        if self.debug:
+            print "Effect and Probability val: ", effect, proba
         action = self.affectWithProbability(action, effect)
         tempState = [state[0],state[1]]
         if action == "up":
@@ -84,21 +87,27 @@ class MDP():
             tempState[1] -= 1
         elif action == "stay":
             action = 'stay'
-            # print "Choosing to stay because of failure"
+            if self.debug:
+                print "Choosing to stay because of failure"
         else:
-            # print "Invalid Action"
+            if self.debug:
+                print "Invalid Action"
             return state
         
         if self.isValid(tempState):
-            # print "Action chosen: ", action
+            if self.debug:
+                print "Action chosen: ", action
             return tempState
         else:
-            # print action, " Transitioning to Invalid state, choosing to STAY"
+            if self.debug:
+                print action, " Transitioning to Invalid state, choosing to STAY"
             return state
 
     def printBoard(self, state, stateCounter=-1, reward=-1):
-        # print "\nSTATE: ", stateCounter
-        # print "----------------"
+        if self.debug:
+            print "\nSTATE: ", stateCounter
+        if self.debug:
+            print "----------------"
         for i in range(self.dimensions):
             currentRow = ""
             for j in range(self.dimensions):
@@ -113,9 +122,10 @@ class MDP():
                 elif self.board.grid[i][j] == Cells.End:
                     currentRow += " [] "
             print currentRow
-        # if reward != -1:
-        #     print "REWARD incurred: ", reward
-        # print "----------------\n"
+        if self.debug:
+            if reward != -1:
+                print "REWARD incurred: ", reward
+            print "----------------\n"
 
     def RewardFunction(self, s_t, a_t, s_t_1, time_step=0):
         '''
@@ -130,20 +140,6 @@ class MDP():
             reward += 10*(self.gamma**time_step)
         return reward
 
-    def runEpisode(self, policy='uniform'):
-        s_t = self.getInitialState()
-        incurredReward = 0
-        while(not self.isTerminalState(s_t)):
-            # self.printBoard(s_t, stateCounter, incurredReward)
-            a_t = self.getActionFromPolicy(s_t, policy=policy)
-            s_t_1 = self.TransitionFunction(s_t, a_t)
-            r_t = self.RewardFunction(s_t, a_t, s_t_1, stateCounter)
-            s_t = s_t_1
-            incurredReward += r_t
-            stateCounter += 1
-        # self.printBoard(s_t, stateCounter, incurredReward)
-        return incurredReward, simulation_statistics
-
     def dumpData(self, data, policy):
         pkl.dump(data, open(str(len(data))+"_Episodes_"+policy+".pkl", 'w'))
         with open(str(len(data))+"_Episodes_"+policy+".csv", 'w') as data_file:
@@ -151,11 +147,31 @@ class MDP():
             for idx, val in enumerate(data):
                 csv_writer.writerow([str(idx+1), str(val)])
         print "Saving dump to: ", str(len(data))+"_Episodes_"+"_Discount_"+str(self.gamma)+"_"+policy+".csv"
-    
+
+    def runEpisode(self, policy='uniform'):
+        s_t = self.getInitialState()
+        incurredReward = 0
+        stateCounter = 0
+        while(not self.isTerminalState(s_t)):
+            if self.debug:
+                self.printBoard(s_t, stateCounter, incurredReward)
+            a_t = self.getActionFromPolicy(s_t, policy=policy)
+            s_t_1 = self.TransitionFunction(s_t, a_t)
+            r_t = self.RewardFunction(s_t, a_t, s_t_1, stateCounter)
+            s_t = s_t_1
+            incurredReward += r_t
+            stateCounter += 1
+            if stateCounter % 1000 == 0:
+                print "At state: ", stateCounter-1
+                print "Reward in current episode: ", incurredReward 
+        if self.debug:
+            self.printBoard(s_t, stateCounter, incurredReward)
+        return incurredReward
+
     def evaluate(self, theta_k, num_episodes):
         reward = 0
         for episode in range(num_episodes):
-            curr_reward, _ = self.runEpisode(policy=theta_k)
+            curr_reward  = self.runEpisode(policy=theta_k)
             reward += curr_reward
             if episode % num_episodes/10 == 0:
                 print "At episode: ", episode
@@ -186,5 +202,5 @@ class MDP():
 
 if __name__ == "__main__":
     board = Board(5)
-    mdp = MDP(board, 0.8, 0.05, 0.05, 0.1, 0.9)
+    mdp = MDP(board, 0.8, 0.05, 0.05, 0.1, 0.9, False)
     mdp.learn_policy_bbo(5, 3, 10, 1e-4, 2)
